523851686a478c44de1c6bd926a1d4a16f55e94e
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-04-22 06:57:49 -0500

    add: starship COPR

--- Scripts/install_pre.sh
+++ Scripts/install_pre.sh
@@ -18,11 +18,15 @@ flg_DryRun=${flg_DryRun:-0}
  echo "Installing COPR repos for hyprland, nwg-shell, fonts, and VSCode"
  sudo dnf copr enable solopasha/hyprland
  sudo dnf copr enable tofik/nwg-shell
+ sudo dnf copr enable atim/starship
+
  sudo dnf install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
  sudo dnf makecache
 
 # grub
